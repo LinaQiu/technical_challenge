32259f57a8df4fc1e36e13ee90285535e16202d7
LinaQiu <qln19940518@gmail.com>
2018-01-20 22:24:10 -0500

    Tested page layouts with faked data.

--- dashboard/scripts/app.py
+++ dashboard/scripts/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
-# from data import Jobs
+from data import Jobs
 from flask_mysqldb import MySQL
 from functools import wraps
 
 app = Flask(__name__)
 
 # Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'mysql-host'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'rallyteam'
 app.config['MYSQL_DB'] = 'challenge'
@@ -15,11 +15,11 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 ## Test used for no db case
-# Jobs = Jobs()
+Jobs = Jobs()
 
 # Index
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
 
 
@@ -32,43 +32,67 @@ def about():
 # Jobs
 @app.route('/jobs')
 def jobs():
-    # Create cursor
-    cur = mysql.connection.cursor()
+    # # Create cursor
+    # cur = mysql.connection.cursor()
 
-    # Get jobs from db (jobs is the table name)
-    result = cur.execute("SELECT * FROM jobs")
+    # # Get jobs from db (jobs is the table name)
+    # result = cur.execute("SELECT * FROM jobs")
 
-    jobs = cur.fetchall()
+    # jobs = cur.fetchall()
 
-    if result > 0:
-        return render_template('jobs.html', jobs=Jobs)
-    else:
-        msg = 'No Jobs Found'
-        return render_template('jobs.html', msg=msg)
-    # Close connection
-    cur.close()
+    # if result > 0:
+    #     return render_template('jobs.html', jobs=Jobs)
+    # else:
+    #     msg = 'No Jobs Found'
+    #     return render_template('jobs.html', msg=msg)
+    # # Close connection
+    # cur.close()
 
+    ## Use faked data to test the page layout and route mechanism
+    return render_template('jobs.html', jobs=Jobs)
 
 #Single Job
 @app.route('/job/<string:id>/')
 def job(id):
-	return render_template('job.html')
-    # Create cursor
-    cur = mysql.connection.cursor()
+    # # Create cursor
+    # cur = mysql.connection.cursor()
 
-    # Get job
-    result = cur.execute("SELECT * FROM jobs WHERE id = %s", [id])
+    # # Get job
+    # result = cur.execute("SELECT * FROM jobs WHERE id = %s", [id])
 
-    job = cur.fetchone()
+    # job = cur.fetchone()
     
-    SELECT expressions_and_columns FROM table_name
-	[WHERE some_condition_is_true]
-	[ORDER BY some_column [ASC | DESC]]
-	[LIMIT offset, rows]
+    # similar_job_results = cur.execute("SELECT * FROM jobs WHERE (company = %s) AND (location = %s)", (job.company, job.location))
+    # similar_jobs = similar_job_results.fetchall()
 
-    similar_job_results = cur.execute("SELECT * FROM jobs WHERE (company = %s) AND (location = %s)", ([job.company], [job.location]))
+    # if similar_job_results>0:
+    #     return render_template('job.html', job=job, similar_jobs=similar_jobs)
+    # else:    
+    #     return render_template('job.html', job=job)
 
-    return render_template('job.html', job=job, similar_jobs=similar_jobs)
+    ## Use faked data to test the page layout and route mechanism
+    job = Jobs[int(id)]
+    similar_jobs = getsimilar(int(id))
+    if len(similar_jobs)>0:
+        return render_template('job.html', job=job, similar_jobs=similar_jobs)
+    else:
+        return render_template('job.html', job=job)
+    
+
+def getsimilar(job_id):
+    """ Helper function to extract similar jobs for the faked data.
+    """
+    similar_jobs = []
+    curr_job_company = Jobs[job_id]['company']
+    curr_job_location = Jobs[job_id]['location']
+    for i in range(len(Jobs)):
+        if i is not job_id:
+            job = Jobs[i]
+            if job['company'] == curr_job_company \
+                and job['location'] == curr_job_location:
+                similar_jobs.append(job)
+    return(similar_jobs)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- dashboard/scripts/data.py
+++ dashboard/scripts/data.py
@@ -20,6 +20,119 @@ def Jobs():
             'title':'Engineer',
             'location':'US',
             'description':'test for job dashboard'
+        },
+        {
+            'id': 4,
+            'company':'Google',
+            'title':'Sale',
+            'location':'Canada',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 5,
+            'company':'Microsoft',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 6,
+            'company':'Google',
+            'title':'Sale',
+            'location':'Canada',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 7,
+            'company':'Microsoft',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 8,
+            'company':'Google',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 9,
+            'company':'Google',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 1,
+            'company':'Google',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 2,
+            'company':'Google',
+            'title':'Sale',
+            'location':'Canada',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 3,
+            'company':'Microsoft',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 4,
+            'company':'Google',
+            'title':'Sale',
+            'location':'Canada',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 5,
+            'company':'Microsoft',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 6,
+            'company':'Google',
+            'title':'Sale',
+            'location':'Canada',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 7,
+            'company':'Microsoft',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 8,
+            'company':'Google',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 9,
+            'company':'Google',
+            'title':'Engineer',
+            'location':'US',
+            'description':'test for job dashboard'
+        },
+        {
+            'id': 10,
+            'company':'Google',
+            'title':'Engineer',
+            'location':'China',
+            'description':'test for job dashboard'
         }
+
     ]
     return jobs

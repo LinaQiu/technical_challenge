bcc9eac22780c633761a68c9ffa3c9713afc2d4d
LinaQiu <qln19940518@gmail.com>
2018-01-21 00:00:55 -0500

    Dashboard with MySQL backend (though not working yet, due to the mysql container issue.) Commented testing code with faked data.

--- dashboard/scripts/app.py
+++ dashboard/scripts/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
-from data import Jobs
+# from data import Jobs
 from flask_mysqldb import MySQL
 from functools import wraps
 
 app = Flask(__name__)
 
 # Config MySQL
-app.config['MYSQL_HOST'] = 'mysql-host'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'rallyteam'
 app.config['MYSQL_DB'] = 'challenge'
@@ -14,8 +14,8 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
 
-## Test used for no db case
-Jobs = Jobs()
+# ## Test used for no db case
+# Jobs = Jobs()
 
 # Index
 @app.route('/')
@@ -32,51 +32,51 @@ def about():
 # Jobs
 @app.route('/jobs')
 def jobs():
-    # # Create cursor
-    # cur = mysql.connection.cursor()
+    # Create cursor
+    cur = mysql.connection.cursor()
 
-    # # Get jobs from db (jobs is the table name)
-    # result = cur.execute("SELECT * FROM jobs")
+    # Get jobs from db (jobs is the table name)
+    result = cur.execute("SELECT * FROM jobs")
 
-    # jobs = cur.fetchall()
+    jobs = cur.fetchall()
 
-    # if result > 0:
-    #     return render_template('jobs.html', jobs=Jobs)
-    # else:
-    #     msg = 'No Jobs Found'
-    #     return render_template('jobs.html', msg=msg)
-    # # Close connection
-    # cur.close()
+    if result > 0:
+        return render_template('jobs.html', jobs=Jobs)
+    else:
+        msg = 'No Jobs Found'
+        return render_template('jobs.html', msg=msg)
+    # Close connection
+    cur.close()
 
-    ## Use faked data to test the page layout and route mechanism
-    return render_template('jobs.html', jobs=Jobs)
+    # ## Use faked data to test the page layout and route mechanism
+    # return render_template('jobs.html', jobs=Jobs)
 
 #Single Job
 @app.route('/job/<string:id>/')
 def job(id):
-    # # Create cursor
-    # cur = mysql.connection.cursor()
+    # Create cursor
+    cur = mysql.connection.cursor()
 
-    # # Get job
-    # result = cur.execute("SELECT * FROM jobs WHERE id = %s", [id])
+    # Get job
+    result = cur.execute("SELECT * FROM jobs WHERE id = %s", [id])
 
-    # job = cur.fetchone()
+    job = cur.fetchone()
     
-    # similar_job_results = cur.execute("SELECT * FROM jobs WHERE (company = %s) AND (location = %s)", (job.company, job.location))
-    # similar_jobs = similar_job_results.fetchall()
+    similar_job_results = cur.execute("SELECT * FROM jobs WHERE (company = %s) AND (location = %s)", (job.company, job.location))
+    similar_jobs = similar_job_results.fetchall()
 
-    # if similar_job_results>0:
-    #     return render_template('job.html', job=job, similar_jobs=similar_jobs)
-    # else:    
-    #     return render_template('job.html', job=job)
-
-    ## Use faked data to test the page layout and route mechanism
-    job = Jobs[int(id)]
-    similar_jobs = getsimilar(int(id))
-    if len(similar_jobs)>0:
+    if similar_job_results>0:
         return render_template('job.html', job=job, similar_jobs=similar_jobs)
-    else:
+    else:    
         return render_template('job.html', job=job)
+
+    # ## Use faked data to test the page layout and route mechanism
+    # job = Jobs[int(id)]
+    # similar_jobs = getsimilar(int(id))
+    # if len(similar_jobs)>0:
+    #     return render_template('job.html', job=job, similar_jobs=similar_jobs)
+    # else:
+    #     return render_template('job.html', job=job)
     
 
 def getsimilar(job_id):
